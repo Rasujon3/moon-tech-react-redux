5d8958b82bc707babd9de3049286c77c76a4ae8e
Rasujon3 <rasujon3@gmail.com>
2022-11-30 01:34:01 +0600

    05 Handling remove functionality.

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
 import { useDispatch } from "react-redux";
-import { addToCart } from "../redux/actionCreators/actionCreators";
+import {
+  addToCart,
+  removeFromCart,
+} from "../redux/actionCreators/actionCreators";
 import { ADD_TO_CART } from "../redux/actionTypes/actionTypes";
+import { RiDeleteBin2Line } from "react-icons/ri";
+import { useLocation } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
   const dispatch = useDispatch();
+  const { pathname } = useLocation();
 
   return (
     <div
@@ -29,18 +35,31 @@ const ProductCard = ({ product }) => {
         </ul>
       </div>
       <div className="flex gap-2 mt-5">
-        <button
-          onClick={() => dispatch(addToCart(product))}
-          className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
-        >
-          Add to cart
-        </button>
-        <button
-          title="Add to wishlist"
-          className="bg-indigo-500  py-1 px-2 rounded-full"
-        >
-          <BiListPlus className="text-white" />
-        </button>
+        {!pathname.includes("cart") && (
+          <button
+            onClick={() => dispatch(addToCart(product))}
+            className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
+          >
+            Add to cart
+          </button>
+        )}
+        {pathname.includes("cart") && (
+          <button
+            onClick={() => dispatch(removeFromCart(product))}
+            className="bg-red-500 flex justify-center items-center gap-2 rounded-full py-1 px-2 flex-1 text-white text-bold"
+          >
+            <RiDeleteBin2Line size={20} />
+            <p className="text-lg">Remove</p>
+          </button>
+        )}
+        {!pathname.includes("cart") && (
+          <button
+            title="Add to wishlist"
+            className="bg-indigo-500  py-1 px-2 rounded-full"
+          >
+            <BiListPlus className="text-white" />
+          </button>
+        )}
       </div>
     </div>
   );
